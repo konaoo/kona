480413b40d3f6b53f959bbcbf9d2d8deeedf06d3
kona <kona@konadeMacBook-Pro.local>
2026-02-06 14:09:32 +0800

    test: fix flutter CI tests and track assets images dir

--- flutter/test/app_state_smoke_test.dart
+++ flutter/test/app_state_smoke_test.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tool/providers/app_state.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+  });
+
   test('AppState formatAmount hides when toggled', () {
     final state = AppState();
     final visible = state.formatAmount(1234, prefix: '¥');
