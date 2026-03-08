961c58557bdd9073d22ab5bf96fbea80d57ab0b2
kona <kona@konadeMacBook-Air.local>
2026-03-08 13:28:27 +0800

    fix: 放开注册邀请码到10位

--- web/src/pages/app/AppLoginPage.vue
+++ web/src/pages/app/AppLoginPage.vue
@@ -75,7 +75,7 @@
               </div>
               <div class="input-wrap">
                 <span class="input-icon"><svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg></span>
-                <input v-model.trim="inviteCode" class="form-input invite" type="text" placeholder="XXXXXX" maxlength="8" @input="inviteCode = inviteCode.toUpperCase()"/>
+                <input v-model.trim="inviteCode" class="form-input invite" type="text" placeholder="XXXXXXXXXX" maxlength="10" @input="inviteCode = inviteCode.toUpperCase()"/>
               </div>
             </div>
             <div class="input-group">
